c04c6b133f2e4f1f25d7dcce0bc9aefeab2a525d
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-04-11 19:40:58 -0400

    git add succeed and fail functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,34 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  // console.log(item);
+  if (item.enhancement < 20) {
+    let newItem = {
+      name: item.name,
+      enhancement: item.enhancement + 1,
+      durability: item.durability
+    }
+    return newItem;
+  }
+  
 }
 
 function fail(item) {
-  return { ...item };
+  let newItem = {
+    name: item.name,
+    enhancement: item.enhancement,
+    durability: item.durability
+  }
+  if (item.enhancement < 15) {
+    newItem.durability =  item.durability - 5
+  }
+  else if (item.enhancement >= 15 && item.enhancement < 16) {
+    newItem.durability = item.durability - 10
+  }
+  else {
+    newItem.durability = item.durability -  1
+  }
+  return newItem;
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,55 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const knife = {
+    name: 'Knife',
+    enhancement: 5,
+    durability: 10
+}
+// const expected = 
+describe('the enhancer module', () => {
+    describe('the succeed function', () => {
+        it('should return an item with all properties set to max', () => {
+            expect(enhancer.succeed(knife)).toEqual({
+                name: knife.name,
+                enhancement: knife.enhancement + 1,
+                durability: knife.durability
+            })
+        })
+    })
+})
+
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+const shield1 = {
+    name: 'Shield',
+    enhancement: 14,
+    durability: 5
+}
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+const shield2 = {
+    name: 'Shield',
+    enhancement: 15,
+    durability: 20
+}
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
+
+describe('the enhancer module', () => {
+    describe('the succeed function', () => {
+        it('should return an item with enhacement changed based on durability', () => {
+            expect(enhancer.fail(shield1)).toEqual({
+                name: shield1.name,
+                enhancement: shield1.enhancement,
+                durability: 0
+            })
+        })
+        it('should return an item with enhacement changed based on durability', () => {
+            expect(enhancer.fail(shield2)).toEqual({
+                name: shield2.name,
+                enhancement: shield2.enhancement,
+                durability: 10
+            })
+        })
+    })
+})
